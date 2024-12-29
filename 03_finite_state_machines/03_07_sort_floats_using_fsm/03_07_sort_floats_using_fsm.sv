baa377b6737be8ec918188915d22376d8a1660ac
egor <egorkokonin557@gmail.com>
2024-12-29 20:06:53 +0300

    I did the 7th and 8th exercises of the 3rd homework assignment

--- 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
+++ 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
@@ -36,6 +36,91 @@ module sort_floats_using_fsm (
     // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
     // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
 
+    logic [0:2][FLEN-1:0] array;
 
+    enum logic [2:0]
+    {
+        S0 = 3'd0,
+        S1 = 3'd1,
+        S2 = 3'd2,
+        S3 = 3'd3,
+        S4 = 3'd4
+    } state, next_state;
 
+    always_comb 
+    begin
+        next_state = state;
+        array      = sorted;
+
+        err = f_le_err;
+
+        f_le_a = '0;
+        f_le_b = '0;
+
+        valid_out = err;
+
+        case (state)
+            S0: 
+            begin
+                if (valid_in) 
+                begin
+                    next_state = S1;
+                    array = unsorted;
+                end
+            end
+
+            S1: 
+            begin
+                f_le_a = sorted[0];
+                f_le_b = sorted[1];
+
+                if (~f_le_res)
+                    array = { sorted [1] , sorted [0], sorted [2] };
+                
+                next_state = S2;
+            end
+
+            S2:                                                                 // after state S2, the largest element will be in 3rd place.
+            begin
+                f_le_a = sorted[1];
+                f_le_b = sorted[2];
+
+                if (~f_le_res)
+                    array = { sorted [0] , sorted [2], sorted [1] };
+
+                next_state = S3;
+            end
+
+            S3:
+            begin
+                f_le_a = sorted[0];
+                f_le_b = sorted[1];
+
+                if (~f_le_res)
+                    array = { sorted [1], sorted [0], sorted[2] };
+
+                next_state = S4;
+            end
+
+            S4: 
+            begin
+                valid_out = '1;
+                next_state = S0;
+            end
+        endcase
+    end
+
+    always_ff @ (posedge clk)
+        if (rst | err)
+        begin
+            state  <= S0;
+            sorted <= {'0, '0, '0};
+        end
+        else
+        begin
+            state  <= next_state;
+            sorted <= array;
+        end
+            
 endmodule
+
--- 03_finite_state_machines/03_07_sort_floats_using_fsm/testbench.sv
+++ 03_finite_state_machines/03_07_sort_floats_using_fsm/testbench.sv
@@ -233,7 +233,7 @@ module testbench;
             // Uncomment the following line
             // to generate a VCD file and analyze it using GTKwave
 
-            // $dumpvars;
+            $dumpvars;
         `endif
 
         run ();
